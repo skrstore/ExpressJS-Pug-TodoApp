b402857056e136f9e1fbc1fd1223cc4a052eb0eb
Sachin Kumar Rajput <31756244+skrmain@users.noreply.github.com>
2022-12-30 18:26:12 +0530

    [refactor] router syntax (#3)
    - combine get, post routes
    - update bootstrap version in pug
    - move user to auth app

--- src/apps/todo/todo.routes.js
+++ src/apps/todo/todo.routes.js
@@ -1,45 +1,44 @@
-const express = require('express');
-const router = express.Router();
+const { Router } = require('express');
 
-const { checkAuth } = require('./../../middleware');
 const { get, post, patch, delete1 } = require('../../services/api');
 
-// home
-router.get('/', checkAuth, async (req, res) => {
-  const response = await get('todo', req.session.token);
-  res.render('home', {
-    todos: response.data.data,
-    user: { email: req.session.email },
+const router = Router();
+
+router
+  .route('/')
+  .get(async (req, res) => {
+    const response = await get('todo', req.session.token);
+    res.render('todos', {
+      todos: response.data.data,
+      user: { email: req.session.email },
+    });
+  })
+  .post(async (req, res) => {
+    await post('todo', { title: req.body.todo }, req.session.token);
+
+    res.redirect('/');
   });
-});
 
-// add new todo
-router.post('/addtodo', checkAuth, async (req, res) => {
-  await post('todo', { title: req.body.todo }, req.session.token);
+// update todo
+router
+  .route('/update/:id')
+  .get(async (req, res) => {
+    const response = await get('todo/' + req.params.id, req.session.token);
+    let user = { email: req.session.email };
 
-  res.redirect('/');
-});
-
-// update todo get
-router.get('/update/:id', checkAuth, async (req, res) => {
-  const response = await get('todo/' + req.params.id, req.session.token);
-  let user = { email: req.session.email };
-
-  res.render('update', { data: response.data.data, user: user });
-});
-
-// update todo post
-router.post('/update/:id', checkAuth, async (req, res) => {
-  await patch(
-    'todo/' + req.params.id,
-    { title: req.body.todo },
-    req.session.token
-  );
-  res.redirect('/');
-});
+    res.render('update', { data: response.data.data, user: user });
+  })
+  .post(async (req, res) => {
+    await patch(
+      'todo/' + req.params.id,
+      { title: req.body.todo },
+      req.session.token
+    );
+    res.redirect('/');
+  });
 
 // delete todo
-router.get('/delete/:id', checkAuth, async (req, res) => {
+router.get('/delete/:id', async (req, res) => {
   await delete1('todo/' + req.params.id, req.session.token);
   res.redirect('/');
 });
